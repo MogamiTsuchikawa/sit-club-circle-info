a5391598d439c33c81541f14bdf71634cafa30f3
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2020-05-14 15:31:30 +0900

    move secret_key to local_settings.py

--- sit_cci/sit_cci/settings.py
+++ sit_cci/sit_cci/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!%bg_nnjj*xg(pzq_)#1!7h#ubxt*&#%-4*b5dq2qcr@4%kdh4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
